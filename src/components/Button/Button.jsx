2cd3c75b8209349b4b19db824d2ee0a8f0ae491d
Mavinxukr <mavinx.office@gmail.com>
2020-05-26 14:38:49 +0300

    first commit

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import styles from './Button.scss';
+
+const Button = ({ children, classNameWrapper, onClick, href, type, viewType, disabled, active }) => {
+  const TagName = href ? 'a' : 'button';
+
+  const classNameForButton = cx(cx(styles.button, classNameWrapper), {
+    [styles.buttonFormDisabled]: viewType === 'formButton' && disabled,
+    [styles.buttonFormActive]: viewType === 'formButton' && active,
+  });
+
+  return (
+    <TagName
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
+      className={classNameForButton}
+    >
+      {children}
+    </TagName>
+  )
+};
+
+Button.propTypes = {
+  children: PropTypes.node,
+  classNameWrapper: PropTypes.string,
+  onClick: PropTypes.func,
+  href: PropTypes.string,
+  type: PropTypes.string,
+  viewType: PropTypes.oneOf(['formButton']),
+  disabled: PropTypes.bool,
+  active: PropTypes.bool,
+};
+
+export default Button;
---
+++ src/components/Wappers/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      hello
+    </div>
+  );
+}
+
+export default App;
